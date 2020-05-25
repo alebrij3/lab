0e54110bcf15a7866dc335e739719ae164ed539c
Alex <ajf@fastmail.com>
2020-05-25 11:34:25 -0500

    LikeButton exercise

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,5 +1,38 @@
-function log(message) {
-    console.log(message);
+// Type Assertion
+let message;
+message = 'abc';
+let endsWithC = message.endsWith('c');
+let alsoEndsWithC = message.endsWith('c');
+// implement the like
+// class button has the number of likes and the state
+// aloow user to set the initial number of likes
+class LikeButton {
+    constructor(_liked = false, _likes = 0) {
+        this._liked = _liked;
+        this._likes = _likes;
+    }
+    click() {
+        if (this._liked == false) {
+            this._liked = true;
+            this._likes++;
+        }
+        else {
+            this._liked = false;
+            this._likes--;
+        }
+    }
+    get likes() {
+        return this._likes;
+    }
+    set likes(x) {
+        this._likes = x;
+    }
 }
-var message = "Hello World";
-log(message);
+let button = new LikeButton(false, 100);
+console.log(button);
+button.click();
+console.log(button);
+button.click();
+console.log(button);
+button.click();
+console.log(button);
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -10,3 +10,35 @@ interface Book {
   title: string,
   author: string,
 }
+
+// implement the like
+// class button has the number of likes and the state
+// aloow user to set the initial number of likes
+class LikeButton {
+  constructor(private _liked: boolean = false, public _likes: number = 0) {
+  }
+  click() {
+    if (this._liked == false) {
+      this._liked = true;
+      this._likes++;
+    } else {
+      this._liked = false;
+      this._likes--;
+    }
+  }
+  get likes() {
+    return this._likes;
+  }
+  set likes(x: number) {
+    this._likes = x;
+  }
+}
+let button = new LikeButton(false, 100);
+
+console.log(button);
+button.click();
+console.log(button);
+button.click();
+console.log(button);
+button.click();
+console.log(button);
