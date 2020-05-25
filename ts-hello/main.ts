10e891fccbbb0196e7896c2faa6c312786e6279e
Alex <ajf@fastmail.com>
2020-05-25 10:25:25 -0500

    Interfaces

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -3,3 +3,10 @@ let message;
 message = 'abc';
 let endsWithC = (<string>message).endsWith('c');
 let alsoEndsWithC = (message as string).endsWith('c');
+
+// Interfaces
+interface Book {
+  id: number,
+  title: string,
+  author: string,
+}
